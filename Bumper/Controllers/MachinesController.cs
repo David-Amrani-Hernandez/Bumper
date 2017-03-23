0d694aa0eedd38bf3a10f35e61b3769477cfafe5
david.amrani <david.amrani@11paths.com>
2017-03-23 22:21:39 +0100

    First commit

---
+++ Bumper/Controllers/MachinesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Bumper;
+
+namespace Bumper.Controllers
+{
+    public class MachinesController : Controller
+    {
+        private BumperEntities db = new BumperEntities();
+
+        // GET: Machines
+        public ActionResult Index()
+        {
+            return View(db.machine.ToList());
+        }
+
+        // GET: Machines/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            machine machine = db.machine.Find(id);
+            if (machine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(machine);
+        }
+
+        // GET: Machines/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Machines/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,provider,instance,tag,domain,publicips,privateips,environment")] machine machine)
+        {
+            if (ModelState.IsValid)
+            {
+                db.machine.Add(machine);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(machine);
+        }
+
+        // GET: Machines/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            machine machine = db.machine.Find(id);
+            if (machine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(machine);
+        }
+
+        // POST: Machines/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,provider,instance,tag,domain,publicips,privateips,environment")] machine machine)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(machine).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(machine);
+        }
+
+        // GET: Machines/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            machine machine = db.machine.Find(id);
+            if (machine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(machine);
+        }
+
+        // POST: Machines/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            machine machine = db.machine.Find(id);
+            db.machine.Remove(machine);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Bumper/Models/FormAnalysis.cs
@@ -0,0 +1,15 @@
+﻿namespace Bumper.Models
+{
+    public class FormAnalysis
+    {
+        public string tag { get; set; }
+
+        public string secgroup { get; set; }
+        public string cloudwatch { get; set; }
+        public string snapshot { get; set; }
+        public string header { get; set; }
+        public string servicestatus { get; set; }
+        public string connections { get; set; }
+        public string publishedkeys { get; set; }
+    }
+}
